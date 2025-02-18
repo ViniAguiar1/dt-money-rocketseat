bdd97c7df0be0869016275607675d77395554778
ViniAguiar1 <vinicius.aguiar1@icloud.com>
2025-02-18 10:14:32 -0300

    feat: implementação do modal de nova transação com Radix UI, adição do campo de pesquisa na tela principal e atualização do README com regras de negócios e funcionais

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,8 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton, HeaderContainerLogo } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog"
 import logoImg from "../../assets/logo.svg";
+import { NewTransactionModal } from "../NewTransactionModal";
+
 export function Header() {
     return (
         <HeaderContainer>
@@ -8,7 +11,13 @@ export function Header() {
                     <img src={logoImg} alt="" />
                     <h1>DT Money</h1>
                 </HeaderContainerLogo>
-                <NewTransactionButton>Nova transação</NewTransactionButton>
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova transação</NewTransactionButton>
+                    </Dialog.Trigger>
+                    <NewTransactionModal />
+                </Dialog.Root>
+
             </HeaderContent>
         </HeaderContainer>
     );
